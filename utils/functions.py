15a96b9d4d0bac65f6efd626bf9d75c35a9818de
Lenoxomorph <github@lenoxc.com>
2022-06-13 20:36:46 -0400

    CSV Function Creation

--- utils/functions.py
+++ utils/functions.py
@@ -15,3 +15,8 @@ def search_csv(key, path):
         for row in reader:
             if row[reader.fieldnames[0]] == key:
                 return row[reader.fieldnames[1]]
+
+
+def append_csv(item, path):
+    with open(path, "a") as file:
+        file.write(item)
