2c3bdbebb58839393bb6d047c7500c8378af5425
Lenoxomorph <github@lenoxc.com>
2022-06-15 10:43:50 -0400

    Rearrange Code

--- cogs/Customization/utils.py
+++ cogs/Customization/utils.py
@@ -1,1 +1,6 @@
-pass
+from utils.errors import GuildPermissionError
+
+
+def is_admin(author):
+    if not author.guild_permissions.administrator:
+        raise GuildPermissionError("YOU CAN NOT CHANGE GUILD PREFIX WITHOUT ADMINISTRATOR PRIVILEGES")
