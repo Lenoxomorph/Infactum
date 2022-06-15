c9184545b5d1486e148958de552a8ae620dedf21
Lenoxomorph <github@lenoxc.com>
2022-06-15 12:32:50 -0400

    New CSV Function Creation

--- utils/functions.py
+++ utils/functions.py
@@ -1,3 +1,6 @@
+import shutil
+from tempfile import NamedTemporaryFile
+
 import discord
 import csv
 
@@ -10,7 +13,7 @@ async def try_delete(message):
 
 
 def search_csv(key, path):
-    with open(path, "r") as file:
+    with open(path, "r", newline='') as file:
         reader = csv.DictReader(file, delimiter=",")
         for row in reader:
             if row[reader.fieldnames[0]] == key:
@@ -18,5 +21,21 @@ def search_csv(key, path):
 
 
 def append_csv(item, path):
-    with open(path, "a") as file:
-        file.write(item)
+    with open(path, "a", newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(item)
+
+
+def edit_csv(item, path):
+    temp_file = NamedTemporaryFile('w+t', delete=False, newline='')  # Delete False
+
+    with open(path, 'r', newline='') as csv_file, temp_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        writer = csv.writer(temp_file, delimiter=',')
+        for row in reader:
+            if row[0] == item[0]:
+                continue
+            writer.writerow(row)
+
+    shutil.move(temp_file.name, path)
+    append_csv(item, path)
