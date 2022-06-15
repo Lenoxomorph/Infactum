930a54851880864d088e511211757554af98e795
Lenoxomorph <github@lenoxc.com>
2022-06-15 10:43:56 -0400

    Rearrange Code

--- utils/checks.py
+++
@@ -1,6 +0,0 @@
-from utils.errors import GuildPermissionError
-
-
-def is_admin(author):
-    if not author.guild_permissions.administrator:
-        raise GuildPermissionError("YOU CAN NOT CHANGE GUILD PREFIX WITHOUT ADMINISTRATOR PRIVILEGES")
