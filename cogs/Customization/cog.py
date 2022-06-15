a0d22018912a482e86f254d40992f47be01905bb
Lenoxomorph <github@lenoxc.com>
2022-06-15 12:32:33 -0400

    Prefix Command Creation

--- cogs/Customization/cog.py
+++ cogs/Customization/cog.py
@@ -2,7 +2,8 @@ import d20
 import discord
 from discord.ext import commands
 
-from utils.checks import is_admin
+from .utils import is_admin
+from utils.functions import edit_csv
 
 
 class Customization(commands.Cog):
@@ -13,9 +14,15 @@ class Customization(commands.Cog):
     @commands.guild_only()
     async def prefix(self, ctx, prefix: str = None):
         """"""  # TODO Add Description
+
+        guild_id = str(ctx.guild.id)
         if prefix is None:
             return await ctx.send(f"CURRENT PREFIX - {ctx.prefix}")
 
         is_admin(ctx.author)
 
-        print("New Prefix" + prefix)
+        self.bot.prefixes[guild_id] = prefix
+
+        edit_csv((guild_id, prefix), "db/prefixes.csv")
+
+        await ctx.send(f"```ini\n[CHANGED SERVER PREFIX TO: {prefix}]\n```")
