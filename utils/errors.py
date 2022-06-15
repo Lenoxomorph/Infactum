84eb336a629c4afaedfcce6b1262c5b79170de2e
Lenoxomorph <github@lenoxc.com>
2022-06-15 10:41:46 -0400

    Error Creation

---
+++ utils/errors.py
@@ -0,0 +1,13 @@
+class InfactumException(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
+class ExternalImportError(InfactumException):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
+class GuildPermissionError(InfactumException):
+    def __init__(self, msg):
+        super().__init__(msg)
