aff7d7508f8094f7216505953c82c4183fd6d30e
Lenoxomorph <github@lenoxc.com>
2022-06-16 17:41:25 -0400

    List Function Creation

--- utils/functions.py
+++ utils/functions.py
@@ -39,3 +39,31 @@ def edit_csv(item, path):
 
     shutil.move(temp_file.name, path)
     append_csv(item, path)
+
+
+def is_in_list(item, path):
+    with open(path, "r") as file:
+        next(file)
+        for line in file:
+            if line.strip() == item:
+                return True
+    return False
+
+
+def append_list(item, path):
+    with open(path, "a") as file:
+        file.write(f"\n{item}")
+
+
+def flat_map_list(func, path):
+    with open(path, "r") as file:
+        next(file)
+        for line in file:
+            func(line.strip())
+
+
+def reset_list(path):
+    with open(path, "r") as file:
+        title = file.readline().strip()
+    with open(path, "w") as file:
+        file.write(title)
