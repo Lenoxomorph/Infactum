c39a95a05478cee9be5495fc7c0f2af6231f1e4b
Lenoxomorph <github@lenoxc.com>
2022-06-16 17:41:11 -0400

    Code Rearrangement

--- cogs/Roller/cog.py
+++ cogs/Roller/cog.py
@@ -2,9 +2,9 @@ import d20
 import discord
 from discord.ext import commands
 
-from utils.dice import MainStringifier, MultiRollContext
+from utils.dice import MainStringifier
 from utils.functions import try_delete
-from .utils import string_search_adv, mention_user
+from .utils import string_search_adv, mention_user, roll_many
 
 
 class Roller(commands.Cog):
@@ -27,48 +27,4 @@ class Roller(commands.Cog):
     async def rr(self, ctx, iterations: int, *, dice):
         """"""  # TODO Add Description
         dice, adv = string_search_adv(dice)
-        await self._roll_many(ctx, iterations, dice, adv=adv)
-
-    @commands.command(name="randchar", aliases=["randomcharacter"])
-    async def randchar(self, ctx):  # TODO Add Leveled Rand Char
-        """"""  # TODO Add Description
-        await self._roll_many(ctx, 6, "4d6kh3")
-
-    @staticmethod
-    async def _roll_many(ctx, iterations, roll_str, dc=None, adv=None):
-        if iterations < 1 or iterations > 100:
-            return await ctx.send("Too many or too few iterations.")
-        if adv is None:
-            adv = d20.AdvType.NONE
-        results = []
-        successes = 0
-        ast = d20.parse(roll_str, allow_comments=True)
-        roller = d20.Roller(context=MultiRollContext())
-
-        for _ in range(iterations):
-            res = roller.roll(ast, advantage=adv)
-            if dc is not None and res.total >= dc:
-                successes += 1
-            results.append(res)
-
-        if dc is None:
-            header = f"**Results:** *Rolling {iterations} iterations*"
-            footer = f"**Total:** {sum(o.total for o in results)}"
-        else:
-            header = f"Rolling {iterations} iterations, DC {dc}..."
-            footer = f"{successes} successes, {sum(o.total for o in results)} total."
-
-        if ast.comment:
-            header = f"{ast.comment}: {header}"
-
-        result_strs = "\n".join(str(o) for o in results)
-
-        out = f"{header}\n{result_strs}\n{footer}"
-
-        if len(out) > 1500:
-            one_result = str(results[0])
-            out = f"{header}\n{one_result}\n[{len(results) - 1} results omitted for output size.]\n{footer}"
-
-        await try_delete(ctx.message)
-        await ctx.send(f"{mention_user(ctx.author)}"
-                       f"\n{out}", allowed_mentions=discord.AllowedMentions(users=[ctx.author]))
+        await roll_many(ctx, iterations, dice, adv=adv)
