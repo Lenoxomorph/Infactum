0544ce21393a231fe2e84eb2e9996051c411f6da
Lenoxomorph <github@lenoxc.com>
2022-06-21 21:00:26 -0400

    Naming System

--- utils/config.py
+++ utils/config.py
@@ -5,3 +5,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 DEFAULT_PREFIX = os.getenv("DEFAULT_PREFIX", ">")
 
 DEFAULT_EMOJI = os.getenv("DEFAULT_EMOJI", ":game_die:")
+
+SYSTEM_NAME = os.getenv("SYSTEM_NAME", "NEW SYSTEM")
+
+SYSTEM_ABV = os.getenv("SYSTEM_ABV", "NEWSYS")
