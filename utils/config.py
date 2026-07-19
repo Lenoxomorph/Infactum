746d0c384bcf766c1b0beda9a94eebec599ecd12
Lenoxomorph <github@lenoxc.com>
2026-07-19 15:20:30 +0000

    Cleanup and dotenv

---
+++ utils/config.py
@@ -0,0 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+
+DEFAULT_PREFIX = os.getenv("DEFAULT_PREFIX", ">")
+
+DEFAULT_EMOJI = os.getenv("DEFAULT_EMOJI", ":game_die:")
+
+SYSTEM_NAME = os.getenv("SYSTEM_NAME", "NEW SYSTEM")
+
+SYSTEM_ABV = os.getenv("SYSTEM_ABV", "NEWSYS")
